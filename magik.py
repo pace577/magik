1e9a1f21b67feab1d931cc2e2142c4f47db3657e
Prabhat <pace577@protonmail.com>
2021-01-26 13:21:30 +0530

    Added DEBUG. Removed unnecessary code.

--- magik.py
+++ magik.py
@@ -5,6 +5,7 @@ import os
 import sys
 from vars import *  # Contains customization variables
 
+DEBUG = False
 
 ## Help text
 USAGE_TEXT = """
@@ -44,6 +45,8 @@ def open_link(subject, link_type):
         reader = csv.DictReader(f, delimiter=',')
         for row in reader:
             if row[SUBJECT] == subject:
+                if DEBUG:
+                    print("Opening link with browser...")
                 os.system(BROWSER + " " + row[link_type]) #command to open the link
 
 
@@ -51,15 +54,6 @@ def get_link_type(link_type_arg):
     """Returns the sppropriate csv header for the link type (live
     lectures/recorded lecutures/assignments) from the argument provided to
     the command when executed"""
-    # if link_type_arg in LINK_LIVE_ARGS_LIST:
-    #     return LIVE
-    # elif link_type_arg in LINK_RECORDED_ARGS_LIST:
-    #     return RECORDED
-    # elif link_type_arg in LINK_ASSIGNMENTS_ARGS_LIST:
-    #     return ASSIGNMENTS
-    # else:
-    #     print("Link type not available!")
-    #     return -1
     for i in range(len(LINK_TYPE_ARGS_LIST)):
         if link_type_arg in LINK_TYPE_ARGS_LIST[i]:
             return LINK_TYPE_LIST[i]
@@ -90,7 +84,8 @@ else:
             link_type = get_link_type(sys.argv[3])
         
         if subject != -1 and link_type != -1:
-            print("Opening", subject, link_type)
+            if DEBUG:
+                print("Opening", subject, link_type)
             open_link(subject, link_type)
         else:
             print("ERROR: subject or link_type not found")
--- vars.py
+++ vars.py
@@ -7,19 +7,13 @@ FILE = "test.csv"
 
 # csv file headings. change these or change the csv file headings to make things work
 SUBJECT = "subject"
+
+# Link types (CSV file headings for columns containing links)
 LINK_TYPE_LIST = ["live_lectures", "recorded_lectures", "assignments"]
-# LIVE = "live_lectures"
-# RECORDED = "recorded_lectures"
-# ASSIGNMENTS = "assignments"
 LINK_TYPE_ARGS_LIST = [["live_lectures", "lecture", "live", "l", "zoom"],
                        ["recorded_lectures", "recorded", "r"],
                        ["assignments", "assignment", "a"]]
 
-# Subjects names
+# Subject names (CSV file first column rows)
 SUBJECT_LIST = ["ps", "cv"] #must match the entry in csv file
 SUBJECT_ARGS_LIST = [["ps", "PS"], ["cv", "CV"]] #can enter any of these in the command line
-
-# Link types
-# LINK_LIVE_ARGS_LIST = ["live_lectures", "lecture", "live", "l", "zoom"]
-# LINK_RECORDED_ARGS_LIST = ["recorded_lectures", "recorded", "r"]
-# LINK_ASSIGNMENTS_ARGS_LIST = ["assignments", "assignment", "a"]
